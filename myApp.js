28b73cc52f268595c952c63c045f9f2044b68a36
Stuart Elliott <77296091+stue2607@users.noreply.github.com>
2024-03-31 22:20:08 +0100

    fixed app.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');  // Import helmet before using it
+// Use helmet middleware
 app.use(helmet.hidePoweredBy());
 app.disable('x-powered-by');
-const helmet = require('helmet');
 app.use(helmet());
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
 
